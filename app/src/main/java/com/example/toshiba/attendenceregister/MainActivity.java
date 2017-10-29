6cabef36da8f98143efb8846ac08169bc9f8b555
KUMAR ABHISHEK <abhishek18296@gmail.com>
2017-10-30 00:03:31 +0530

    First Commit
    The attendence register view for teacher's activity.

---
+++ app/src/main/java/com/example/toshiba/attendenceregister/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.toshiba.attendenceregister;
+
+import android.content.Intent;
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ButtonBarLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private AlertDialog dialog;
+    private EditText urlTxt;
+    private String url;
+    private  ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        url = "https://192.168.43.249/andApp/admin1.php";
+        //url = "https://www.flipkart.com";
+        webView = (WebView) this.findViewById(R.id.webViewOne);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.loadUrl(url);
+        webView.setWebViewClient(new SSLTolerentWebViewClient());
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                progressBar.setProgress(progress);
+                if (progress == 100) {
+                    progressBar.setVisibility(View.GONE);
+
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+
+                }
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.reload();
+                Snackbar.make(view, "Page is being refreshed!", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.goTo:
+                Toast.makeText(getApplicationContext(), "Go To Selected", Toast.LENGTH_LONG).show();
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.loadurl, null);
+                urlTxt = (EditText) mView.findViewById(R.id.eturl);
+                Button go = (Button) mView.findViewById(R.id.goButton);
+                mBuilder.setView(mView);
+                dialog = mBuilder.create();
+                dialog.show();
+
+                go.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Go button clicked!", Toast.LENGTH_LONG).show();
+                        dialog.dismiss();
+                        webView.loadUrl(urlTxt.getText().toString());
+                    }
+                });
+
+                return true;
+            case R.id.search:
+                Toast.makeText(getApplicationContext(), "Search Selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.logOut:
+                Toast.makeText(getApplicationContext(), "Log Out Selected", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
+
+class SSLTolerentWebViewClient extends WebViewClient {
+    public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+        handler.proceed(); // Ignore SSL certificate errors
+    }
+}
